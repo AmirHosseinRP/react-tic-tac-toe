61894cf8f60a08d65c563b3490a1172fedb930de
AmirHosseinRP <rezapanahhf1@gmail.com>
2022-08-17 16:12:20 +0430

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import Board from "./Components/Board";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    useEffect(()=>{
+        document.body.style.backgroundColor='cyan';
+        document.body.style.display='flex';
+        document.body.style.justifyContent='center';
+        document.body.style.alignItems='center';
+
+    })
+    return (
+        <Board/>
+    );
 }
 
 export default App;
